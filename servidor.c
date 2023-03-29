c2a3fb79fe031871d0e6896c516fca8674844d03
ElVaquilla <ashley.cuevas889@gmail.com>
2023-03-29 16:38:07 +0200

    Version1

--- servidor.c
+++ servidor.c
@@ -8,125 +8,131 @@
 #include <pthread.h>
 #include <mysql.h>
 int main(int argc, char *argv[]) {
-	
 	MYSQL *conn;
 	int err;
-	// Estructura especial para almacenar resultados de consultas 
+	char contra[20];
+	char jugador[20];
 	MYSQL_RES *resultado;
 	MYSQL_ROW row;
-	char nombre[20];
-	ID[10];
-	char consulta [80];
-	//Creamos una conexion al servidor MYSQL 
 	conn = mysql_init(NULL);
 	if (conn==NULL) {
 		printf ("Error al crear la conexion: %u %s\n", 
 				mysql_errno(conn), mysql_error(conn));
 		exit (1);
 	}
-	//inicializar la conexion
-	conn = mysql_real_connect (conn, "localhost","user", "pass", "Comando_SQL",0, NULL, 0);
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "bd", 0, NULL, 0);
 	if (conn==NULL) {
 		printf ("Error al inicializar la conexion: %u %s\n", 
 				mysql_errno(conn), mysql_error(conn));
 		exit (1);
 	}
-	// consulta SQL para obtener una tabla con todos los datos de la base de datos
-	err=mysql_query (conn, "SELECT * FROM Comando_SQL");
-	if (err!=0) {
-		printf ("Error al consultar datos de la base %u %s\n",
-				mysql_errno(conn), mysql_error(conn));
-		exit (1);
-	}
-	//recogemos el resultado de la consulta. El resultado de la consulta se devuelve en una variable del tipo puntero a
-	//MYSQL_RES tal y como hemos declarado anteriormente. Se trata de una tabla virtual en memoria que es la copia de la tabla real en disco.
-	resultado = mysql_store_result (conn);
-	// El resultado es una estructura matricial en memoria en la que cada fila contiene los datos de una persona.
-	// Ahora obtenemos la primera fila que se almacena en una variable de tipo MYSQL_ROW
-	row = mysql_fetch_row (resultado);
-	// En una fila hay tantas columnas como datos tiene una
-	// persona. En nuestro caso hay tres columnas: Nombre(row[0])y ID(row[1])
-	if (row == NULL)
-		printf ("No se han obtenido datos en la consulta\n");
-	else
-		while (row !=NULL) {
-			// la columna 2 contiene una palabra que es la edad
-			// la convertimos a entero 
-			edad = atoi (row[1]);
-			// las columnas 0 y 1 contienen DNI y nombre 
-			printf ("Nombre: %s, ID: %d\n", row[0], row[1]);
-			// obtenemos la siguiente fila
-			row = mysql_fetch_row (resultado);
-	}
-		
-	// Ahora vamos a buscar el nombre de la persona cuyo  es uno dado por el usuario
-		printf ("Introduce el nombre de la persona que desea ingresar\n"); 
-		scanf ("%s", nombre);
-		// construimos la consulta SQL
-		strcpy (consulta,"SELECT nombre FROM personas WHERE nombre = '"); 
-		strcat (consulta, dni);
-		strcat (consulta,"'");
-		// hacemos la consulta 
-		err=mysql_query (conn, consulta); 
-		if (err!=0) {
-			printf ("Error al consultar datos de la base %u %s\n",
-					mysql_errno(conn), mysql_error(conn));
-			exit (1);
-		}
-		//recogemos el resultado de la consulta 
-		resultado = mysql_store_result (conn); 
-		row = mysql_fetch_row (resultado);
-		if (row == NULL)
-			printf ("No se ha encontrado al usuario en la base de datos\n");
-		else
-	    // El resultado debe ser una matriz con una sola fila y una columna que contiene el nombre
-			printf ("Nombre de la persona: %s\n", row[0] );
-		// cerrar la conexion con el servidor MYSQL 
-		mysql_close (conn);
-		exit(0);
-}
-
-		
-	int sock_conn, sock_listen;
-	struct sockaddr_in serv_adr;
 	
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
 	// INICIALITZACIONS
 	// Obrim el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		printf("Error creant socket");
 	// Fem el bind al port
 	
+	
 	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
 	serv_adr.sin_family = AF_INET;
+	
 	// asocia el socket a cualquiera de las IP de la m?quina. 
 	//htonl formatea el numero que recibe al formato necesario
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	// establecemos el puerto de escucha
-	serv_adr.sin_port = htons(9050);
+	serv_adr.sin_port = htons(9061);
 	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
 		printf ("Error al bind");
 	
 	if (listen(sock_listen, 3) < 0)
 		printf("Error en el Listen");
 	
-	contador =0;
-	pthread_t thread;
-	i=0;
+	int i;
+	// Bucle infinito
 	for (;;){
 		printf ("Escuchando\n");
 		
 		sock_conn = accept(sock_listen, NULL, NULL);
 		printf ("He recibido conexion\n");
-		
-		sockets[i] =sock_conn;
 		//sock_conn es el socket que usaremos para este cliente
 		
-		// Crear thead y decirle lo que tiene que hacer
-		
-		pthread_create (&thread, NULL, AtenderCliente,&sockets[i]);
-		i=i+1;
-		
+		int terminar =0;
+		// Entramos en un bucle para atender todas las peticiones de este cliente
+		//hasta que se desconecte
+		while (terminar ==0)
+		{
+			// Ahora recibimos la petici?n
+			ret=read(sock_conn,peticion, sizeof(peticion));
+			printf ("Recibido\n");
+			
+			// Tenemos que a\ufff1adirle la marca de fin de string 
+			// para que no escriba lo que hay despues en el buffer
+			peticion[ret]='\0';
+			
+			
+			printf ("Peticion: %s\n",peticion);
+			
+			// vamos a ver que quieren
+			char *p = strtok( peticion, "/");
+			int codigo =  atoi (p);
+			// Ya tenemos el c?digo de la petici?n
+			char nombre[20];
+			char contrase\ufff1a[20];
+			
+			if (codigo !=0)
+			{
+				p = strtok( NULL, "/");
+				strcpy (nombre, p);
+				
+				p = strtok( NULL, "/");
+				strcpy (contra, p);
+				
+				p = strtok( NULL, "/");
+				strcpy (jugador, p);
+				
+				printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+			}
+			
+			if (codigo ==0) //petici?n de desconexi?n
+				terminar=1;
+			else if (codigo ==1) //mirar si el usuario y contrase\uffc3\uffb1a son correctos
+			{	
+				char consulta [80];
+				strcpy (consulta,"SELECT Usuarios.Contrasena FROM Usuarios WHERE Usuarios.nombre = '");
+				strcat (consulta, nombre);
+				strcat (consulta,"'");
+				
+				err=mysql_query (conn, consulta);
+				
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					exit (1);
+				}
+				resultado = mysql_store_result (conn);
+				int result= strcmp(resultado, contra);
+				if (result == 0)
+					sprintf (respuesta, "Correcto");
+				else
+					sprintf (respuesta, "Incorrecto");
+				
+			}
+			
+			if (codigo !=0)
+			{
+				
+				printf ("Respuesta: %s\n", respuesta);
+				// Enviamos respuesta
+				write (sock_conn,respuesta, strlen(respuesta));
+			}
+		}
+		// Se acabo el servicio para este cliente
+		close(sock_conn); 
 	}
 	
 }
-
